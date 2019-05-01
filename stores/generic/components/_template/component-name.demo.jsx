97a2daa83cb0e5a3d5afb243a0f4eab479b473cd
Chuanqi <henrysun918@gmail.com>
2019-04-30 20:22:22 -0700

    add template component

---
+++ stores/generic/components/_template/component-name.demo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ComponentName } from './component-name';
+
+export function ComponentNameDemo() {
+  return (
+    <StyledDiv>
+      <ComponentName />
+    </StyledDiv>
+  );
+}
+
+export default ComponentNameDemo;
+
+const StyledDiv = styled.div``;
---
+++ stores/generic/components/_template/component-name.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { space, color } from '../../styles';
+import ImageName from '../../assets/images/ImageName.svg';
+
+export function ComponentName() {
+  return <ComponentRoot>hello world</ComponentRoot>;
+}
+
+const ComponentRoot = styled.div``;
